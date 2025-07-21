4bec90b72793906a432b261a782f16f20f59a560
“david <davzy007@gmail.com>
2025-07-21 18:55:46 +0100

    added an function two cherk if username is already takengit add .git add .

---
+++ handlers/auth/nickname_available.go
@@ -0,0 +1,54 @@
+package auth
+
+import (
+	"breadcrumb-backend-go/models"
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+)
+
+var ddbClient *dynamodb.Client
+var tableName string
+
+func init() {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		panic(fmt.Sprintf("unable to load SDK config, %v", err))
+	}
+	ddbClient = dynamodb.NewFromConfig(cfg)
+	tableName = os.Getenv("USERS_TABLE")
+	if tableName == "" {
+		panic("USERS_TABLE environment variable not set")
+	}
+}
+
+func HandleNicknameAvailable(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	nickname := req.PathParameters["nickname"]
+	if nickname == "" {
+		return models.InvalidRequestErrorResponse(""), nil
+	}
+
+	out, err := ddbClient.Query(ctx, &dynamodb.QueryInput{
+		TableName:              aws.String(tableName),
+		IndexName:              aws.String("NicknameIndex"),
+		KeyConditionExpression: aws.String("nickname = :nick"),
+		ExpressionAttributeValues: map[string]types.AttributeValue{
+			":nick": &types.AttributeValueMemberS{Value: nickname},
+		},
+		Limit: aws.Int32(1),
+	})
+
+	if err != nil {
+		return models.ServerSideErrorResponse(""), nil
+	}
+
+	exists := len(out.Items) > 0
+
+	return models.SuccessfulRequestResponse(fmt.Sprintf("%v", exists), false), nil
+}
---
+++ models/server_responses.go
@@ -0,0 +1,61 @@
+package models
+
+import (
+	"encoding/json"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+// buildResponse builds a standard API Gateway proxy response
+func buildResponse(statusCode int, body map[string]string) events.APIGatewayProxyResponse {
+	jsonBody, _ := json.Marshal(body)
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: statusCode,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body: string(jsonBody),
+	}
+}
+
+// responseMessage formats a message or error into a response
+func responseMessage(statusCode int, message string) events.APIGatewayProxyResponse {
+	var msgKey string
+	if statusCode >= 300 {
+		msgKey = "error"
+	} else {
+		msgKey = "message"
+	}
+
+	return buildResponse(statusCode, map[string]string{
+		msgKey: message,
+	})
+}
+
+func InvalidRequestErrorResponse(msg string) events.APIGatewayProxyResponse {
+	if msg == "" {
+		msg = "Invalid request body."
+	}
+	return responseMessage(400, msg)
+}
+
+func ServerSideErrorResponse(msg string) events.APIGatewayProxyResponse {
+	if msg == "" {
+		msg = "An error has occurred on our end, try again."
+	}
+	return responseMessage(500, msg)
+}
+
+func SuccessfulRequestResponse(msg string, createdResource bool) events.APIGatewayProxyResponse {
+	if msg == "" {
+		msg = "Request successful"
+	}
+
+	sCode := 200
+	if createdResource {
+		sCode = 201
+	}
+
+	return responseMessage(sCode, msg)
+}
