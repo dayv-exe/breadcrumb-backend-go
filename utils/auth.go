fa7d7e7971ecb5b53390cacc33b432da6bb4f4d7
“david <davzy007@gmail.com>
2025-08-21 21:40:46 +0100

    debugging auth utils

--- utils/auth.go
+++ utils/auth.go
@@ -1,17 +1,25 @@
 package utils
 
-import "github.com/aws/aws-lambda-go/events"
+import (
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+)
 
 func GetAuthUserId(req *events.APIGatewayProxyRequest) string {
 	claims, ok := req.RequestContext.Authorizer["claims"].(map[string]interface{})
 	if !ok {
 		// missing claims
+		log.Println("missing claims")
 		return ""
 	}
+
+	log.Println(claims)
 
 	sub, ok := claims["sub"].(string)
 	if !ok {
 		// missing sub
+		log.Println("missing sub")
 		return ""
 	}
 
